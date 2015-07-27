4527d992cbded50495df3496b5dc94f679d9cade
oukiar <oukiar@gmail.com>
2015-07-27 09:19:08 -0500

    avance weekend

--- orgboat.py
+++ orgboat.py
@@ -1,12 +1,16 @@
 
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.popup import Popup
+from kivy.uix.dropdown import DropDown
 
 from kivy.properties import ObjectProperty, StringProperty
 
 from datepicker import DatePicker
 
 import os
+
+from datetime import date, datetime, time, timedelta
+import pytz
 
 try:
     from devslib.utils import MessageBoxTime
@@ -20,6 +24,44 @@ except:
 
 from kivy.core.window import Window
 Window.set_icon("orgboat.png")
+
+class SessionMenu(DropDown):
+    pass
+
+class Profile(BoxLayout):
+    def __init__(self, **kwargs):
+        super(Profile, self).__init__(**kwargs)
+
+        #self.jobactivities.updateList()
+
+class TaskStatusMenu(BoxLayout):
+    def on_changestatus(self, text):
+        print text
+
+        tarea = Tareas.Query.get(objectId=self.parent.taskID)
+        tarea.Status = text
+        tarea.save()
+
+        app.root.battleplan.updateList()
+
+        self.parent.remove_widget(self)
+
+    def on_options(self):
+        print "Opening options"
+
+        tm = TaskMenu(taskItem=self.parent)
+        tm.txt_tarea.text = self.parent.tasktext.text
+
+        #obtener tarea
+        tarea = Tareas.Query.get(objectId=self.parent.taskID)
+        #inicializar valores en GUI
+        tm.lst_status.text = tarea.Status
+
+        tm.open()
+
+    def on_close(self):
+        print "Closing"
+        self.parent.remove_widget(self)
 
 class NewJobActivity(Popup):
     txt_title = ObjectProperty()
@@ -35,10 +77,24 @@ class NewJobActivity(Popup):
 
         self.dismiss()
 
-class JobActivities(BoxLayout):
+        #add to the gui
+        act = JobActivity()
+        act.title.text = jobactivity.Title
+        act.description.text = jobactivity.Description
+        app.root.profile.jobactivitiestable.jobactivitieslist.add_widget(act)
+
+class JobActivity(BoxLayout):
+    pass
+
+class JobActivitiesTable(BoxLayout):
     def updateList(self):
-        for jobact in app.root.jobactivities:
+        for jobact in app.root.jobactivitiescache:
             print jobact.Title
+
+            act = JobActivity()
+            act.title.text = jobact.Title
+            act.description.text = jobact.Description
+            self.jobactivitieslist.add_widget(act)
 
 class TaskMenu(Popup):
 
@@ -73,6 +129,10 @@ class BattlePlanItem(BoxLayout):
     taskID = StringProperty()
 
     def open_menu(self):
+        menu = TaskStatusMenu()
+        self.add_widget(menu, len(self.children)-1)
+        return
+
         tm = TaskMenu(taskItem=self)
         tm.txt_tarea.text = self.tasktext.text
         
@@ -97,6 +157,7 @@ class BattlePlan(BoxLayout):
         tarea.Task = self.tasktext.text
         tarea.Status = "Pending"
         tarea.PUser = app.root.user
+        tarea.createdAtLocalTime = datetime.now(pytz.timezone('America/Mexico_City') )
         tarea.save()
         
         
@@ -110,7 +171,53 @@ class BattlePlan(BoxLayout):
         #self.tasktext.focus = True
         
     def updateList(self):
-        tareas = Tareas.Query.filter(PUser__in=[app.root.user]).order_by("-createdAt")
+
+
+        #filter
+        if self.filter.text == 'Today':
+
+            pub_date = datetime.now(pytz.timezone('America/Mexico_City') )
+            min_pub_date_time = datetime.combine(pub_date, time.min)
+            max_pub_date_time = datetime.combine(pub_date, time.max)
+
+            print min_pub_date_time, max_pub_date_time
+
+            tareas = Tareas.Query.filter(PUser__in=[app.root.user],
+                                         createdAtLocalTime__gte=min_pub_date_time,
+                                         createdAtLocalTime__lte=max_pub_date_time).order_by("-createdAt")
+        elif self.filter.text == 'This week':
+
+            dt = datetime.now(pytz.timezone('America/Mexico_City') )
+
+            start = dt - timedelta(days = dt.weekday())
+            end = start + timedelta(days = 6)
+
+            tareas = Tareas.Query.filter(PUser__in=[app.root.user],
+                                         createdAtLocalTime__gte=start,
+                                         createdAtLocalTime__lte=end).order_by("-createdAt")
+        elif self.filter.text == 'Yesterday':
+            dt = datetime.now(pytz.timezone('America/Mexico_City') )
+            dt = dt - timedelta(days = 1)
+            dtmin = datetime.combine(dt, time.min)
+            dtmax = datetime.combine(dt, time.max)
+            print dtmin, dtmax
+            tareas = Tareas.Query.filter(PUser__in=[app.root.user],
+                                         createdAtLocalTime__gte=dtmin,
+                                         createdAtLocalTime__lte=dtmax).order_by("-createdAt")
+        elif self.filter.text == 'This month':
+            tareas = Tareas.Query.filter(PUser__in=[app.root.user], createdAt__in=[datetime.datetime.today()]).order_by("-createdAt")
+        elif self.filter.text == 'Last month':
+            tareas = Tareas.Query.filter(PUser__in=[app.root.user], createdAt__in=[datetime.datetime.today()]).order_by("-createdAt")
+        elif self.filter.text == 'Date interval':
+            tareas = Tareas.Query.filter(PUser__in=[app.root.user], createdAt__in=[datetime.datetime.today()]).order_by("-createdAt")
+        elif self.filter.text == 'Expireds':
+            tareas = Tareas.Query.filter(PUser__in=[app.root.user], Status__in=['Expired']).order_by("-createdAt")
+        elif self.filter.text == 'Pendings':
+            tareas = Tareas.Query.filter(PUser__in=[app.root.user], Status__in=['Pending']).order_by("-createdAt")
+        elif self.filter.text == 'In progress':
+            tareas = Tareas.Query.filter(PUser__in=[app.root.user], Status__in=['In progress']).order_by("-createdAt")
+        elif self.filter.text == 'Dones':
+            tareas = Tareas.Query.filter(PUser__in=[app.root.user], Status__in=['Done']).order_by("-createdAt")
 
         self.tasks.clear()
 
@@ -151,7 +258,8 @@ class Orgboat(BoxLayout):
 
         
     def changeTab(self, tabToShow):
-        self.main.workSpace.remove_widget(self.currentTab)
+        if self.currentTab != None:
+            self.main.workSpace.remove_widget(self.currentTab)
         
         self.currentTab = tabToShow
         self.main.workSpace.add_widget(self.currentTab)
@@ -171,24 +279,26 @@ class Orgboat(BoxLayout):
         self.add_widget(self.main)
 
         #create the sections of the user interface
-        
+
+        self.profile = Profile()
         self.battleplan = BattlePlan()
         self.reports = Reports()
         self.commendations = Commendations()
         self.stats = Stats()
-        
-        self.profile = self.main.profile
-        
+
+        self.currentTab = None
+        self.changeTab(self.profile)
+
+        #self.main.workSpace.add_widget(self.profile)
+
         #add profile section as visible
         #self.main.workSpace.add_widget(self.profile)
-        
-        #save the active section for successfull section change
-        self.currentTab = self.main.profile
 
         #cache de datos
-        self.jobactivities = JobActivities.Query.filter(PUser__in=[app.root.user]).order_by("-createdAt")
+        self.jobactivitiescache = JobActivities.Query.filter(PUser__in=[app.root.user]).order_by("-createdAt")
 
         #inicializar interfaz de usuario
+        self.profile.jobactivitiestable.updateList()
         self.battleplan.updateList()
 
     def openNewJobActivity(self):
